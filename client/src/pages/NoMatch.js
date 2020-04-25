f2aafdde2ee087f7c36a0cb9ec7846de4d9b55c2
Mangenese <blakemangen20@gmail.com>
2020-04-24 18:28:39 -0700

    added the logic for the app.js as well as the pages to display items

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+function NoMatch() {
+    return (
+      <div>
+            <Header>
+                <h1>Error status code: 404.... These are not the droids you are looking for.....</h1>
+                </Header>
+                </div>
+    );
+}
+export default NoMatch;
